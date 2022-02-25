20a8a5f1c83df7377e4b36df1d8bdcd150cf850e
Hariharan A <hari20002010@gmail.com>
2022-02-25 12:19:22 +0530

    Updated codes for pointnet training

---
+++ pointnet/sem_seg/getter.py
@@ -0,0 +1,93 @@
+import argparse
+import math
+import h5py
+import numpy as np
+import tensorflow as tf
+import socket
+
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+import provider
+import tf_util
+from model import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
+parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
+parser.add_argument('--num_point', type=int, default=4096, help='Point number [default: 4096]')
+parser.add_argument('--max_epoch', type=int, default=50, help='Epoch to run [default: 50]')
+parser.add_argument('--batch_size', type=int, default=24, help='Batch Size during training [default: 24]')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
+parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
+parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
+parser.add_argument('--decay_step', type=int, default=300000, help='Decay step for lr decay [default: 300000]')
+parser.add_argument('--decay_rate', type=float, default=0.5, help='Decay rate for lr decay [default: 0.5]')
+parser.add_argument('--test_area', type=int, default=6, help='Which area to use for test, option: 1-6 [default: 6]')
+FLAGS = parser.parse_args()
+
+
+BATCH_SIZE = FLAGS.batch_size
+NUM_POINT = FLAGS.num_point
+MAX_EPOCH = FLAGS.max_epoch
+NUM_POINT = FLAGS.num_point
+BASE_LEARNING_RATE = FLAGS.learning_rate
+GPU_INDEX = FLAGS.gpu
+MOMENTUM = FLAGS.momentum
+OPTIMIZER = FLAGS.optimizer
+DECAY_STEP = FLAGS.decay_step
+DECAY_RATE = FLAGS.decay_rate
+
+LOG_DIR = FLAGS.log_dir
+if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
+os.system('cp model.py %s' % (LOG_DIR)) # bkp of model def
+os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
+LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
+LOG_FOUT.write(str(FLAGS)+'\n')
+
+MAX_NUM_POINT = 4096
+NUM_CLASSES = 13
+
+BN_INIT_DECAY = 0.5
+BN_DECAY_DECAY_RATE = 0.5
+#BN_DECAY_DECAY_STEP = float(DECAY_STEP * 2)
+BN_DECAY_DECAY_STEP = float(DECAY_STEP)
+BN_DECAY_CLIP = 0.99
+
+HOSTNAME = socket.gethostname()
+
+ALL_FILES = provider.getDataFiles('indoor3d_sem_seg_hdf5_data/all_files.txt')
+room_filelist = [line.rstrip() for line in open('indoor3d_sem_seg_hdf5_data/room_filelist.txt')]
+
+# Load ALL data
+data_batch_list = []
+label_batch_list = []
+for h5_filename in ALL_FILES:
+    data_batch, label_batch = provider.loadDataFile(h5_filename)
+    data_batch_list.append(data_batch)
+    label_batch_list.append(label_batch)
+data_batches = np.concatenate(data_batch_list, 0)
+label_batches = np.concatenate(label_batch_list, 0)
+print(data_batches.shape)
+print(label_batches.shape)
+
+test_area = 'Area_'+str(FLAGS.test_area)
+train_idxs = []
+test_idxs = []
+for i,room_name in enumerate(room_filelist):
+    if test_area in room_name:
+        test_idxs.append(i)
+    else:
+        train_idxs.append(i)
+
+train_data = data_batches[train_idxs,...]
+train_label = label_batches[train_idxs]
+test_data = data_batches[test_idxs,...]
+test_label = label_batches[test_idxs]
+print(train_data.shape, train_label.shape)
+print(test_data.shape, test_label.shape)
+
