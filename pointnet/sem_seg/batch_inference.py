2d7dc47e8e530ea5a671f56b64671fb15b7d9096
Hariharan A <hari20002010@gmail.com>
2022-03-11 16:23:36 +0000

    Add files via upload

--- pointnet/sem_seg/batch_inference.py
+++ pointnet/sem_seg/batch_inference.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import rospy
 import argparse
 import os
 import sys
@@ -6,15 +9,18 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
 from model import *
 import indoor3d_util
-
+from sensor_msgs.msg import PointCloud2
+import pcl
+import ros_numpy
+count = 0
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
 parser.add_argument('--batch_size', type=int, default=1, help='Batch Size during training [default: 1]')
 parser.add_argument('--num_point', type=int, default=4096, help='Point number [default: 4096]')
-parser.add_argument('--model_path', required=True, help='model checkpoint file path')
-parser.add_argument('--dump_dir', required=True, help='dump folder path')
-parser.add_argument('--output_filelist', required=True, help='TXT filename, filelist, each line is an output for a room')
-parser.add_argument('--room_data_filelist', required=True, help='TXT filename, filelist, each line is a test room data label file.')
+parser.add_argument('--model_path', default='log/model.ckpt', help='model checkpoint file path')
+parser.add_argument('--dump_dir', default='log/dump', help='dump folder path')
+parser.add_argument('--output_filelist', default='log/output_filelist.txt', help='TXT filename, filelist, each line is an output for a room')
+parser.add_argument('--room_data_filelist', default='metal/area6_data_label.txt', help='TXT filename, filelist, each line is a test room data label file.')
 parser.add_argument('--no_clutter', action='store_true', help='If true, donot count the clutter class')
 parser.add_argument('--visu', action='store_true', help='Whether to output OBJ file for prediction visualization.')
 FLAGS = parser.parse_args()
@@ -36,54 +42,6 @@ def log_string(out_str):
     LOG_FOUT.flush()
     print(out_str)
 
-def evaluate():
-    is_training = False
-     
-    with tf.device('/gpu:'+str(GPU_INDEX)):
-        pointclouds_pl, labels_pl = placeholder_inputs(BATCH_SIZE, NUM_POINT)
-        is_training_pl = tf.placeholder(tf.bool, shape=())
-
-        # simple model
-        pred = get_model(pointclouds_pl, is_training_pl)
-        loss = get_loss(pred, labels_pl)
-        pred_softmax = tf.nn.softmax(pred)
- 
-        # Add ops to save and restore all the variables.
-        saver = tf.train.Saver()
-        
-    # Create a session
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.allow_soft_placement = True
-    config.log_device_placement = True
-    sess = tf.Session(config=config)
-
-    # Restore variables from disk.
-    saver.restore(sess, MODEL_PATH)
-    log_string("Model restored.")
-
-    ops = {'pointclouds_pl': pointclouds_pl,
-           'labels_pl': labels_pl,
-           'is_training_pl': is_training_pl,
-           'pred': pred,
-           'pred_softmax': pred_softmax,
-           'loss': loss}
-    
-    total_correct = 0
-    total_seen = 0
-    fout_out_filelist = open(FLAGS.output_filelist, 'w')
-    for room_path in ROOM_PATH_LIST:
-        out_data_label_filename = os.path.basename(room_path)[:-4] + '_pred.txt'
-        out_data_label_filename = os.path.join(DUMP_DIR, out_data_label_filename)
-        out_gt_label_filename = os.path.basename(room_path)[:-4] + '_gt.txt'
-        out_gt_label_filename = os.path.join(DUMP_DIR, out_gt_label_filename)
-        print(room_path, out_data_label_filename)
-        a, b = eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_filename)
-        total_correct += a
-        total_seen += b
-        fout_out_filelist.write(out_data_label_filename+'\n')
-    fout_out_filelist.close()
-    log_string('all room eval accuracy: %f'% (total_correct / float(total_seen)))
 
 def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_filename):
     error_cnt = 0
@@ -97,6 +55,7 @@ def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_f
         fout = open(os.path.join(DUMP_DIR, os.path.basename(room_path)[:-4]+'_pred.obj'), 'w')
         fout_gt = open(os.path.join(DUMP_DIR, os.path.basename(room_path)[:-4]+'_gt.obj'), 'w')
     fout_data_label = open(out_data_label_filename, 'w')
+    fin_data_label = open(out_data_label_filename, 'r')
     fout_gt_label = open(out_gt_label_filename, 'w')
     
     current_data, current_label = indoor3d_util.room2blocks_wrapper_normalized(room_path, NUM_POINT)
@@ -112,7 +71,10 @@ def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_f
     file_size = current_data.shape[0]
     num_batches = file_size // BATCH_SIZE
     print(file_size)
-
+    x = []
+    y = []
+    z = []
+    I = []
     
     for batch_idx in range(num_batches):
         start_idx = batch_idx * BATCH_SIZE
@@ -124,7 +86,7 @@ def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_f
                      ops['is_training_pl']: is_training}
         loss_val, pred_val = sess.run([ops['loss'], ops['pred_softmax']],
                                       feed_dict=feed_dict)
-
+	
         if FLAGS.no_clutter:
             pred_label = np.argmax(pred_val[:,:,0:12], 2) # BxN
         else:
@@ -138,12 +100,17 @@ def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_f
             pts[:,8] *= max_room_z
             pts[:,3:6] *= 255.0
             pred = pred_label[b, :]
+
             for i in range(NUM_POINT):
                 color = indoor3d_util.g_label2color[pred[i]]
                 color_gt = indoor3d_util.g_label2color[current_label[start_idx+b, i]]
                 if FLAGS.visu:
                     fout.write('v %f %f %f %d %d %d\n' % (pts[i,6], pts[i,7], pts[i,8], color[0], color[1], color[2]))
                     fout_gt.write('v %f %f %f %d %d %d\n' % (pts[i,6], pts[i,7], pts[i,8], color_gt[0], color_gt[1], color_gt[2]))
+                x.append(pts[i,6])
+                y.append(pts[i,7])
+                z.append(pts[i,8])
+                I.append(pred[i] *50)
                 fout_data_label.write('%f %f %f %d %d %d %f %d\n' % (pts[i,6], pts[i,7], pts[i,8], pts[i,3], pts[i,4], pts[i,5], pred_val[b,i,pred[i]], pred[i]))
                 fout_gt_label.write('%d\n' % (l[i]))
         correct = np.sum(pred_label == current_label[start_idx:end_idx,:])
@@ -155,18 +122,101 @@ def eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_f
                 l = current_label[i, j]
                 total_seen_class[l] += 1
                 total_correct_class[l] += (pred_label[i-start_idx, j] == l)
-
-    log_string('eval mean loss: %f' % (loss_sum / float(total_seen/NUM_POINT)))
-    log_string('eval accuracy: %f'% (total_correct / float(total_seen)))
+	
+    #log_string('eval mean loss: %f' % (loss_sum / float(total_seen/NUM_POINT)))
+    #log_string('eval accuracy: %f'% (total_correct / float(total_seen)))
     fout_data_label.close()
     fout_gt_label.close()
+    
+    x = np.array(x)
+    predicted  = np.zeros((x.shape[0] , 6))
+    predicted  = np.zeros(x.shape[0], dtype=[('x', np.float32),('y', np.float32),('z', np.float32),('I', np.int32)])
+    print(x.shape)
+    predicted_msg = PointCloud2()
+    predicted['x'] = x
+    predicted['y'] = np.array(y)
+    predicted['z'] = np.array(z)
+    predicted['I'] = np.array(I)
+    predicted_msg = ros_numpy.msgify(PointCloud2, predicted)
+    predicted_msg.header.frame_id = 'predicted_pointnet'
+    pub = rospy.Publisher("predicted_topic", PointCloud2, queue_size=10)
+    pub.publish(predicted_msg)
+    rospy.loginfo("Msg Published")
+    
+    
+    
+    
+    
     if FLAGS.visu:
         fout.close()
         fout_gt.close()
     return total_correct, total_seen
 
 
-if __name__=='__main__':
+
+
+
+def talker():
     with tf.Graph().as_default():
-        evaluate()
-    LOG_FOUT.close()
+    	  rospy.init_node("Predictor")
+    	  count = 0
+    	  rate = rospy.Rate(10)
+    	  is_training = False
+    	  rospy.loginfo("count")
+    	  rospy.loginfo(count)
+    	  with tf.device('/gpu:'+str(GPU_INDEX)):
+            pointclouds_pl, labels_pl = placeholder_inputs(BATCH_SIZE, NUM_POINT)
+            is_training_pl = tf.placeholder(tf.bool, shape=())
+        # simple model
+            pred = get_model(pointclouds_pl, is_training_pl)
+            loss = get_loss(pred, labels_pl)
+            pred_softmax = tf.nn.softmax(pred)
+        
+        # Add ops to save and restore all the variables.
+            saver = tf.train.Saver()
+
+    
+    # Create a session
+     	    config = tf.ConfigProto()
+    	    config.gpu_options.allow_growth = True
+    	    config.allow_soft_placement = True
+    	    config.log_device_placement = True
+    	    sess = tf.Session(config=config)
+    
+    # Restore variables from disk.
+    	    saver.restore(sess, MODEL_PATH)
+    	    log_string("Model restored.")
+    	  ops = {'pointclouds_pl': pointclouds_pl,
+        'labels_pl': labels_pl,
+        'is_training_pl': is_training_pl,
+        'pred': pred,
+        'pred_softmax': pred_softmax,
+        'loss': loss}
+        
+ 
+    	  while not rospy.is_shutdown():
+    
+    		total_correct = 0
+    		total_seen = 0
+    		fout_out_filelist = open(FLAGS.output_filelist, 'w')
+    		for room_path in ROOM_PATH_LIST:
+        		out_data_label_filename = os.path.basename(room_path)[:-4] + '_pred.txt'
+        		out_data_label_filename = os.path.join(DUMP_DIR, out_data_label_filename)
+        		out_gt_label_filename = os.path.basename(room_path)[:-4] + '_gt.txt'
+        		out_gt_label_filename = os.path.join(DUMP_DIR, out_gt_label_filename)
+        		print(room_path, out_data_label_filename)
+        		a, b = eval_one_epoch(sess, ops, room_path, out_data_label_filename, out_gt_label_filename)
+        		total_correct += a
+        		total_seen += b
+        		fout_out_filelist.write(out_data_label_filename+'\n')
+    		fout_out_filelist.close()
+    		#log_string('all room eval accuracy: %f'% (total_correct / float(total_seen)))
+    		rate.sleep()
+    	  LOG_FOUT.close()
+
+if __name__=='__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
+
